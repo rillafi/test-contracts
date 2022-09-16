93adaa81cd1d5af80253ac0088fbca00d1141fd6
aaronknudtson <aaron.knudtson@outlook.com>
2022-09-16 16:24:04 -0400

    added description to voteresult.txt

--- votes/voteparse.py
+++ votes/voteparse.py
@@ -13,8 +13,11 @@ gg = os.popen(f'cat {path} | grep 0xda190be12ee7c48dbd5ed06d9b13c11ac50e3000c802
 votes=[]
 yes_votes = 0
 no_votes = 0
+description=""
 for r in gg:
   row = asList(r)
+  if (len(row)==40):
+    description=row[16]+row[17]+row[18]+row[19]+row[20]+row[21]
   if (len(row)==14): # length 14 indicates part of vote table, meaning it is a recorded vote
     votes.append({"voter":row[2],"vote":int(row[6]),"vote_weight":int(float(row[10]))})
     if int(row[6])==1:
@@ -22,11 +25,10 @@ for r in gg:
     else:
       no_votes += votes[-1]["vote_weight"]
       
-print("vote history:", votes)
 print(f"Votes yes: {yes_votes} ({yes_votes/(yes_votes+no_votes)*100}%)")
 print(f"Votes no: {no_votes} ({no_votes/(yes_votes+no_votes)*100}%)")
 
 with open("votes/voteresult.json","w") as f:
   f.write(json.dumps(votes))
 with open("votes/voteresult.txt","w") as f:
-  f.write(f"Votes yes: {yes_votes} ({yes_votes/(yes_votes+no_votes)*100}%) \n Votes no: {no_votes} ({no_votes/(yes_votes+no_votes)*100}%)")
+  f.write(f"Description: {description} \nVotes yes: {yes_votes} ({yes_votes/(yes_votes+no_votes)*100}%) \nVotes no: {no_votes} ({no_votes/(yes_votes+no_votes)*100}%)")
